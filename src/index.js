864ec6267a95da59ad8a5bad47772e121e22bb59
Naflo <chaliflorent@gmail.com>
2017-12-03 20:17:19 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class ShoppingList extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>Shopping List {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>Whatsapp</li>
+                    <li>Oculous</li>
+                </ul>
+            </div>
+        );
+    }
+}
